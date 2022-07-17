beab0f94d74f95cca49efb74898e865f0b827a53
andrasatriapratama <andrasatriapratama@gmail.com>
2022-07-17 12:22:29 +0700

    halaman dart

---
+++ dart/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Andra Pratama</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="/favicon_io/site.webmanifest">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+
+<body>
+
+    <section id="main">
+        <nav>
+            <a href="#" class="logo">Andra Pratama</a>
+            <ul class="menu">
+                <li><a href="#">Dart Topics</a></li>
+
+            </ul>
+            <a href="#" class="lang">ID</a>
+        </nav>
+    </section>
+
+</body>
+
+</html>
